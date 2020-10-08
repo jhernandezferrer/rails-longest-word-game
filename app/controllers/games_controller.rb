3eabbd585dc9548804a96daf85d13b1dfe41a95f
Jose Hernandez <pepeh93@hotmail.es>
2020-10-08 11:29:33 +0200

    Added score page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,6 +9,22 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    puts json
+    json['found']
   end
 end
